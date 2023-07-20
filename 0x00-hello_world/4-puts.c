936c123c3273ea9b0abcfea0cf778890ee8554c6
GABFORDTECH <gabfordtech@mail.com>
2023-07-20 00:50:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
-  *main - entry poit
-  *return: always 0 (success)
-  */
+ * main - entry point
+ * Return: always 0 (success)
+ */
 int main(void)
 {
-		puts("\"programming is like building a multilingual puzzle");
-		return (0);
+    puts("\"programming is like building a multilingual puzzle\"");
+    return (0);
 }
