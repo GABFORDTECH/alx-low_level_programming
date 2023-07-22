083dbcf999a6cc93bcfdb004f72d55084393af5b
GABFORDTECH <gabfordtech@mail.com>
2023-07-21 18:26:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,13 @@
  * main - Entry point of the program
  *
  * Description: This function writes a message to the standard error.
- *              The message is "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * The message is "and that piece of art is useful" - Dora Korpar, 2015-10-19
  *
  * Return: Always returns 1 to indicate an error.
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	     sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19") - 1);
+	     sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
