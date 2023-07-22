04dbe25f96c143b4d996b532994fc7c807505041
GABFORDTECH <gabfordtech@mail.com>
2023-07-21 18:23:04 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,16 @@
 #include <unistd.h>
 
-int main(void) {
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-         sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-  return 1;
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function writes a message to the standard error.
+ *              The message is "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *
+ * Return: Always returns 1 to indicate an error.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	     sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19") - 1);
+	return (1);
 }
