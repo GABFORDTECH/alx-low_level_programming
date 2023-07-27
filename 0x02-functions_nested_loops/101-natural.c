b0794fe38e3ae053c2cf073829abd2d2e83d0dd6
GABFORDTECH <gabfordtech@mail.com>
2023-07-26 18:15:32 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-    int limit = 1024;
-    int sum = 0;
-    int i;
+	int limit = 1024;
+	int sum = 0;
+	int i;
 
-    for (i = 0; i < limit; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;
-        }
-    }
+	for (i = 0; i < limit; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
 
-    printf("The sum of all the multiples of 3 or 5 below 1024 is: %d\n", sum);
+	printf("%d\n", sum);
 
-    return (0);
+	return (0);
 }
