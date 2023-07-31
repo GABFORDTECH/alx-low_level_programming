b9e8bf45a1a171555b2d46a2bba3044b56c70207
GABFORDTECH <gabfordtech@mail.com>
2023-07-30 23:59:50 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the largest prime factor of 612852475143
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int i, n = 612852475143;
+
+	for (i = 3; i <= 1782849; i = i + 2)
+	{
+		while ((n % i == 0) && (n != 1))
+		{
+			n = n / i;
+		}
+	}
+
+	printf("%lu\n", n);
+	return (0);
+}
