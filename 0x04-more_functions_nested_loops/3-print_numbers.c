73673a94969c991751124d9340ed70df3ff8c52b
GABFORDTECH <gabfordtech@mail.com>
2023-07-30 21:26:34 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,12 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints numbers from 0 to 9.
+ * main - Check the code.
  *
  * Return: Always 0.
  */
-void print_numbers(void)
+int main(void)
 {
-    char c;
-
-    for (c = '0'; c <= '9'; c++)
-    {
-        _putchar(c);
-    }
-
-    _putchar('\n');
+	print_numbers();
+	return (0);
 }
