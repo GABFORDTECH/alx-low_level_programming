cc987026fa93b4e5ff030e9d4a9be0cb17148a74
GABFORDTECH <gabfordtech@mail.com>
2023-08-01 07:11:56 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,11 +5,13 @@
   * @author anisah
   * @a: swaps and store address of b
   * @b: swaps and store address of a
-  * Return: 0*/
+  * Return: 0
+ */
 
 void swap_int(int *a, int *b)
 {
 	int change;
+
 	change = *b;
 	*b = *a;
 	*a = change;
