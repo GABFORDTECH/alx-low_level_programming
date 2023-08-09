f1b3179d21797cddd37f22c674c7712f32c4e24d
GABFORDTECH <gabfordtech@mail.com>
2023-08-08 18:13:41 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * set_string - sets the value of a pointers to a char.
+  * @s: the pointers.
+  * @to: the char.
+  */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
